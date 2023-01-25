5cede655e3c9cad0e05a89deb6397b8ccc45e4ce
Maycon Douglas <mayconmrqs2001@gmail.com>
2023-01-25 02:59:27 -0300

    Implemented base frontend, mobile and backend app

---
+++ mobile/src/components/Loadind.tsx
@@ -0,0 +1,19 @@
+import { View, ActivityIndicator } from "react-native";
+import React from "react";
+
+const Loading = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#09090A",
+      }}
+    >
+      <ActivityIndicator color="#7C3AED" />
+    </View>
+  );
+};
+
+export default Loading;
